c74011dc6b805abcee45a60b7a3fdb7b3712416f
Chad Smith <chadxsmith@ymail.com>
2015-04-24 09:22:45 -0400

    style changes to the header on cities index page

--- app/controllers/homeless_people_controller.rb
+++ app/controllers/homeless_people_controller.rb
@@ -4,7 +4,10 @@ class HomelessPeopleController < ApplicationController
     @users = User.all
     @user = @users.last
     @user_name = @user.name.capitalize
-    @city = City.find(params[:city_id]) #find the question
+
+
+    @city = City.find(params[:city_id])  
+
     @homeless_people =  @city.homeless_people
     @homeless_person = @homeless_people.sample
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,24 @@ class UsersController < ApplicationController
     end
 
     def create
+       @city = City.find(params[:city_id])
         @user = User.new( user_params )
         @user.save
-        redirect_to city_homeless_people_path(params[:city_id])
+        redirect_to  city_homeless_people_path(@city)
+        # URI Pattern: /cities/:city_id/homeless_people/:id
+        #since params is a hash, we passed over the key "city_id", which
+        #has access to the value @city.id. This specifies the city, each
+        #homeless person belongs to
+
+        #special note: we couldn't simple create @city instance variable
+        #and pass it into the
+
+        #rule:
+
+        # show action vs redirect
+
+        #show
+
     end
 
     private
